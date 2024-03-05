9aa45538463b3f857556d41b2b98ca62f774bcb6
Ismoil Shokirov <ismoil.793@gmail.com>
2024-03-05 15:13:51 -0600

    signup and dashboard

---
+++ app/src/main/java/com/example/hw3_hw4_amazon/Dashboard.kt
@@ -0,0 +1,27 @@
+package com.example.hw3_hw4_amazon
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Dashboard : AppCompatActivity() {
+    private lateinit var goBackBtn: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_dashboard)
+
+
+        goBackBtn = findViewById(R.id.goBack)
+
+        goBackBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
--- app/src/main/java/com/example/hw3_hw4_amazon/MainActivity.kt
+++ app/src/main/java/com/example/hw3_hw4_amazon/MainActivity.kt
@@ -1,16 +1,39 @@
 package com.example.hw3_hw4_amazon
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var signInBtn: Button
+    private lateinit var createAccountBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // Important find inputs, btns after layout creation
+        signInBtn = findViewById(R.id.signInBtn)
+        createAccountBtn = findViewById(R.id.createAccountBtn)
+
+        signInBtn.setOnClickListener {
+            // new Intent(context, java class of file)
+            val intent = Intent(this, Dashboard::class.java)
+            startActivity(intent);
+        }
+
+        createAccountBtn.setOnClickListener {
+            val intent = Intent(this, Signup::class.java)
+            startActivity(intent);
+        }
+    }
+
+    fun test() {
 
     }
 }
